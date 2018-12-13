0e27b73bd2616a5b1227ac5050c2db936bacfe9f
Fellipe Hallanny <fellipehallanny@gmail.com>
2018-12-13 10:10:29 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/fellipehallanny/cursomc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.fellipehallanny.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fellipehallanny.cursomc.domain.Cliente;
+import com.fellipehallanny.cursomc.repositories.ClienteRepository;
+import com.fellipehallanny.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+
+	}
+
+}
